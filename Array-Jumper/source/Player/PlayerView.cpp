8e89f5bd35971c5dd32200f2279d159fd38e1cd5
Aman Kumar <aman.kumar01747@gmail.com>
2025-01-20 11:33:04 +0530

    -Create function to draw player

--- Array-Jumper/source/Player/PlayerView.cpp
+++ Array-Jumper/source/Player/PlayerView.cpp
@@ -1,4 +1,8 @@
 #include"../../header/Player/PlayerView.h"
+#include "../../header/Global/Config.h"
+#include "../../header/Global/ServiceLocator.h"
+
+using namespace Global;
 
 namespace Player
 {
@@ -29,5 +33,40 @@ namespace Player
 	{
 		player_height = 1000.f;
 		player_width = 1000.f;
+	}
+
+	void PlayerView::initializePlayerImage()
+	{
+		player_image->initialize(Config::character_texture_path,
+			player_width,
+			player_height,
+			sf::Vector2f(0, 0));
+	}
+
+	void PlayerView::loadPlayer()
+	{
+		calculatePlayerDimensions();
+		initializePlayerImage();
+	}
+
+	void PlayerView::initialize()
+	{
+		game_window = ServiceLocator::getInstance()->getGraphicService()->getGameWindow();
+		loadPlayer();
+	}
+
+	sf::Vector2f PlayerView::calulcatePlayerPosition()
+	{
+		return sf::Vector2f(0, 0);
+	}
+
+	void PlayerView::updatePlayerPosition()
+	{
+		player_image->setPosition(calulcatePlayerPosition());
+	}
+
+	void PlayerView::drawPlayer()
+	{
+		player_image->render();
 	}
 }
